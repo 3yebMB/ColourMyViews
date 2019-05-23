72895ef9ff4a5f71d2342786d0529c9b0d94d08d
Michael <3yebmb@gmail.com>
2019-05-23 16:36:42 +0500

    init ColourMyViews project

---
+++ app/src/main/java/dev/m13d/colourmyviews/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.m13d.colourmyviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
