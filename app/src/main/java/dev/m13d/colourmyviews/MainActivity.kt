a062f4b3bc8abf47e3f2b0624668d5efa329cfa1
Michael <3yebmb@gmail.com>
2019-05-23 18:27:45 +0500

    add resources, three buttons
    when buttons is clicked text is painted

--- app/src/main/java/dev/m13d/colourmyviews/MainActivity.kt
+++ app/src/main/java/dev/m13d/colourmyviews/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         val clickableViews: List<View> = listOf(box_one_text, box_two_text, box_three_text, box_four_text,
-                                                box_five_text, constraint_layout)
+                                                box_five_text, constraint_layout, red_button, yellow_button, green_button)
 
         for (item in clickableViews) {
             item.setOnClickListener{ makeColoured(it)}
@@ -28,9 +28,14 @@ class MainActivity : AppCompatActivity() {
         when (view.id) {
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
